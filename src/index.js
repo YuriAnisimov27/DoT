ebe0d2802cc8ead0f0c9d4a7f7b81e0b224beb99
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-28 16:58:40 +0300

    feat: add event relics counter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ window.addEventListener('load', () => {
     const gems = document.querySelector('.count__form-gems');
     const ratio = document.querySelector('#ratio');
     const souls = document.querySelector('#souls');
-    const ratioValue = parseFloat(ratio.value);
+    const ratioValue = parseFloat(ratio.value.replace(',', '.'));
     const soulsValue = +souls.value;
 
     if (Number.isNaN(ratioValue)) {
@@ -31,5 +31,49 @@ window.addEventListener('load', () => {
 
     fights.innerHTML = `Боев: <span class="count__form-fightsCount">${fightsCount}</span>`;
     gems.innerHTML = `Красных: <span class="count__form-gemsCount">${gemsCount}</span>`;
+  });
+
+  const eventRelicBtns = document.querySelectorAll('.eventRelic-btnsGroup-btn');
+  const eventRatio = document.querySelector('#ratio-event');
+  const starCountedField = document.querySelector('.eventRecic-counter');
+  const resultEventRelic = document.querySelector('.eventRecic-result');
+  const resetEventData = document.querySelector('.eventRecic-reset');
+
+  function updateResultEventRelic(i) {
+    const ratioEventValue = +(eventRatio.value.replace(',', '.'));
+    switch (i) {
+      case 0:
+        resultEventRelic.innerHTML = +resultEventRelic.textContent
+          + (10 + 10 * 3) * 8 * ratioEventValue;
+        break;
+      case 1:
+        resultEventRelic.innerHTML = +resultEventRelic.textContent
+          + (10 + 10 * 4.5) * 8 * ratioEventValue;
+        break;
+      case 2:
+        resultEventRelic.innerHTML = +resultEventRelic.textContent
+          + (10 + 10 * 9) * 8 * ratioEventValue;
+        break;
+      case 3:
+        resultEventRelic.innerHTML = +resultEventRelic.textContent
+          + (10 + 10 * 18) * 8 * ratioEventValue;
+        break;
+      default:
+        resultEventRelic.innerHTML = +resultEventRelic.textContent
+          + (10 + 10 * 36) * 8 * ratioEventValue;
+        break;
+    }
+  }
+
+  eventRelicBtns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      starCountedField.innerHTML = `${starCountedField.innerHTML} ${btn.innerHTML}`;
+      updateResultEventRelic(index);
+    });
+  });
+
+  resetEventData.addEventListener('click', () => {
+    starCountedField.innerHTML = '';
+    resultEventRelic.textContent = 0;
   });
 });
